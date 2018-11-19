d58fcea55dbf4c15c5323f342d84745d5f89992e
Vytautas Povilaitis <vytautas@veleza.com>
2018-11-19 19:43:54 +0200

    Adds more widget configuration examples

--- Example/Example/Controllers/SurveysPhotosConfigurationViewController.swift
+++ Example/Example/Controllers/SurveysPhotosConfigurationViewController.swift
@@ -12,9 +12,22 @@ import VelezaSDK
 class SurveysPhotosConfigurationViewController: UIViewController {
     
     @IBOutlet var widget: VelezaSurveysPhotosWidget?
+    
+    
+    @IBAction func setWidgetPadding(sender: UISegmentedControl) {
+        widget?.padding = CGFloat(sender.selectedSegmentIndex * 20)
+    }
 
     @IBAction func setPhotoRowCount(sender: UISegmentedControl) {
         widget?.imageRows = sender.selectedSegmentIndex + 1
+    }
+
+    @IBAction func setPhotoSpacing(sender: UISegmentedControl) {
+        widget?.imageSpacing = CGFloat(sender.selectedSegmentIndex * 10)
+    }
+
+    @IBAction func setSurveyCount(sender: UISegmentedControl) {
+        widget?.visibleSurveys = (sender.selectedSegmentIndex + 1) * 2
     }
 
 }
